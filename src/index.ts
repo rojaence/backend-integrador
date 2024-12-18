9a588608659013387830a1767974f6ef2565504f
Ronny Endara <rojaence@gmail.com>
2024-12-18 18:47:38 -0500

    feat(express): configuracion inicial de express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hola mundo')
+import express, { NextFunction, Request, Response } from 'express'
+
+const app = express()
+
+const prefix = "/api"
+const port = 3000
+
+app.get(`${prefix}/auth`, 
+  async (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hola mundo')
+})
+
+app.listen(port, () => {
+  console.log(`El servidor está corriendo en el puerto: ${port}`)
+})
+
+
