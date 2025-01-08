6dbb3a113de4e81c189f758ff76e9e260eeaba8f
Ronny Endara <rojaence@gmail.com>
2025-01-08 18:21:27 -0500

    feat(DOCKER/POSTGRES): :sparkles: Agregado: Configuracion de docker y postgresql con sequelize

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -4,3 +4,9 @@ config()
 
 export const PORT = process.env.PORT ?? 3000
 export const JWT_SECRET = process.env.JWT_SECRET ?? 'secret'
+
+export const DB_USER = process.env.DB_USER!
+export const DB_PASS = process.env.DB_PASS!
+export const DB_NAME = process.env.DB_NAME!
+export const DB_HOST = process.env.DB_HOST!
+export const DB_PORT = process.env.DB_PORT!
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,25 @@ import { PORT } from './environment/env'
 import authRoutes from "./modules/auth/routes"
 import userRoutes from "./modules/user/routes"
 import { ValidationError } from 'express-validation'
+import db from './config/dbOrm'
 
 const app = express()
 
 app.use(express.json())
+
+// Conexion a db
+async function main() {
+  try {
+    await db.authenticate()
+    await db.sync({force: true}) // La sincronización con force en true puede eliminar registros o columnas
+    console.log('Conexión establecida con la base de datos');
+  } catch (error) {
+    console.error('Ocurrió un error al conectarse con la base de datos:', error);
+  }
+}
+
+main()
+
 const prefix = "/api"
 app.use(`${prefix}/auth`, authRoutes)
 app.use(`${prefix}/users`, userRoutes)
